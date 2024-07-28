882886b93159b94130aa78741160f5c544e237e4
CurtoBrull <curto.brull.javier@gmail.com>
2024-07-28 07:22:37 +0000

    chore: Update dependencies and configuration files

--- src/main/java/eu/jacurtobr/sacoauth/config/SecurityConfig.java
+++ src/main/java/eu/jacurtobr/sacoauth/config/SecurityConfig.java
@@ -1,2 +1,28 @@
-package eu.jacurtobr.sacoauth.config;public class SecurityConfig {
+package eu.jacurtobr.sacoauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/oauth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+        )
+                .formLogin( formLogin -> formLogin
+                        .loginPage("/login")
+                        .permitAll()
+                );
+        return http.build();
+    }
+
 }
